16c51a115c1b2ed832114281f1a5cf66771d9e81
Juan Zuluaga <juan.zuluaga@cscgfm.com>
2024-03-01 08:08:26 -0500

    Add dot to error messages

--- Orders/Orders.Frontend/Repositories/HttpResponseWrapper.cs
+++ Orders/Orders.Frontend/Repositories/HttpResponseWrapper.cs
@@ -37,10 +37,10 @@ namespace Orders.Frontend.Repositories
             }
             if (statusCode == HttpStatusCode.Forbidden)
             {
-                return "No tienes permisos para hacer esta operación";
+                return "No tienes permisos para hacer esta operación.";
             }
 
-            return "Ha ocurrido un error inesperado";
+            return "Ha ocurrido un error inesperado.";
         }
     }
 }
