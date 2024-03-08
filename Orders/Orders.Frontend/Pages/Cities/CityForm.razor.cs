1421b50ecec86310c3b2c308bc212bbb5f800d24
Juan Zuluaga <juan.zuluaga@cscgfm.com>
2024-03-08 16:14:44 -0500

    City create & edit

---
+++ Orders/Orders.Frontend/Pages/Cities/CityForm.razor.cs
@@ -0,0 +1,56 @@
+﻿using CurrieTechnologies.Razor.SweetAlert2;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components.Routing;
+using Orders.Shared.Entities;
+
+namespace Orders.Frontend.Pages.Cities
+{
+    public partial class CityForm
+    {
+        private EditContext editContext = null!;
+
+        [EditorRequired, Parameter] public City City { get; set; } = null!;
+        [EditorRequired, Parameter] public EventCallback OnValidSubmit { get; set; }
+        [EditorRequired, Parameter] public EventCallback ReturnAction { get; set; }
+        [Inject] private SweetAlertService SweetAlertService { get; set; } = null!;
+        public bool FormPostedSuccessfully { get; set; }
+
+        protected override void OnInitialized()
+        {
+            editContext = new(City);
+        }
+
+        private async Task OnBeforeInternalNavigationAsync(LocationChangingContext context)
+        {
+            var formWasEdited = editContext.IsModified();
+
+            if (!formWasEdited)
+            {
+                return;
+            }
+
+            if (FormPostedSuccessfully)
+            {
+                return;
+            }
+
+            var result = await SweetAlertService.FireAsync(new SweetAlertOptions
+            {
+                Title = "Confirmación",
+                Text = "¿Deseas abandonar la página y perder los cambios?",
+                Icon = SweetAlertIcon.Question,
+                ShowCancelButton = true
+            });
+
+            var confirm = !string.IsNullOrEmpty(result.Value);
+
+            if (confirm)
+            {
+                return;
+            }
+
+            context.PreventNavigation();
+        }
+    }
+}
