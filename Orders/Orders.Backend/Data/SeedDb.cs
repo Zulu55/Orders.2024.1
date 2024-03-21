9f89c1d0f87165f6a440955a56a3d33bfb11816d
Juan Zuluaga <juan.zuluaga@cscgfm.com>
2024-03-21 18:43:22 -0500

    Seed Db

---
+++ Orders/Orders.Backend/Data/SeedDb.cs
@@ -0,0 +1,48 @@
+﻿
+using Orders.Shared.Entities;
+
+namespace Orders.Backend.Data
+{
+    public class SeedDb
+    {
+        private readonly DataContext _context;
+
+        public SeedDb(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SeedAsync()
+        {
+            await _context.Database.EnsureCreatedAsync();
+            await CheckCountriesAsync();
+            await CheckCatregoriesAsync();
+        }
+
+        private async Task CheckCatregoriesAsync()
+        {
+            if (!_context.Categories.Any())
+            {
+                _context.Categories.Add(new Category { Name = "Tenología" });
+                _context.Categories.Add(new Category { Name = "Mascotas" });
+                _context.Categories.Add(new Category { Name = "Hogar" });
+                _context.Categories.Add(new Category { Name = "Cosmeticos" });
+                _context.Categories.Add(new Category { Name = "Licores" });
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        private async Task CheckCountriesAsync()
+        {
+            if (!_context.Countries.Any())
+            {
+                _context.Countries.Add(new Country { Name = "Colombia" });
+                _context.Countries.Add(new Country { Name = "Perú" });
+                _context.Countries.Add(new Country { Name = "Argentina" });
+                _context.Countries.Add(new Country { Name = "USA" });
+                _context.Countries.Add(new Country { Name = "Italia" });
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
--- Orders/Orders.Backend/Program.cs
+++ Orders/Orders.Backend/Program.cs
@@ -4,7 +4,6 @@ using Orders.Backend.Repositories.Implementations;
 using Orders.Backend.Repositories.Interfaces;
 using Orders.Backend.UnitsOfWork.Implementations;
 using Orders.Backend.UnitsOfWork.Interfaces;
-using Orders.Shared.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,10 +11,24 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=LocalConnection"));
+builder.Services.AddTransient<SeedDb>();
+
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 builder.Services.AddScoped(typeof(IGenericUnitOfWork<>), typeof(GenericUnitOfWork<>));
 
 var app = builder.Build();
+SeedData(app);
+
+void SeedData(WebApplication app)
+{
+    var scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+
+    using (var scope = scopedFactory!.CreateScope())
+    {
+        var service = scope.ServiceProvider.GetService<SeedDb>();
+        service!.SeedAsync().Wait();
+    }
+}
 
 app.UseCors(x => x
     .AllowAnyMethod()
