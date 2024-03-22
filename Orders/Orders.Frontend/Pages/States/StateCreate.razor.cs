9a3c7e4b0b9ae29aa07a921594cb86b824f633b5
Juan Zuluaga <juan.zuluaga@cscgfm.com>
2024-03-21 20:44:21 -0500

    Create states

---
+++ Orders/Orders.Frontend/Pages/States/StateCreate.razor.cs
@@ -0,0 +1,46 @@
+﻿using CurrieTechnologies.Razor.SweetAlert2;
+using Microsoft.AspNetCore.Components;
+using Orders.Frontend.Repositories;
+using Orders.Frontend.Shared;
+using Orders.Shared.Entities;
+
+namespace Orders.Frontend.Pages.States
+{
+    public partial class StateCreate
+    {
+        private State state = new();
+        private FormWithName<State>? stateForm;
+
+        [Parameter] public int CountryId { get; set; }
+        [Inject] private IRepository Repository { get; set; } = null!;
+        [Inject] private NavigationManager NavigationManager { get; set; } = null!;
+        [Inject] private SweetAlertService SweetAlertService { get; set; } = null!;
+
+        private async Task CreateAsync()
+        {
+            state.CountryId = CountryId;
+            var responseHttp = await Repository.PostAsync("/api/states", state);
+            if (responseHttp.Error)
+            {
+                var message = await responseHttp.GetErrorMessageAsync();
+                await SweetAlertService.FireAsync("Error", message, SweetAlertIcon.Error);
+                return;
+            }
+            Return();
+            var toast = SweetAlertService.Mixin(new SweetAlertOptions
+            {
+                Toast = true,
+                Position = SweetAlertPosition.BottomEnd,
+                ShowConfirmButton = true,
+                Timer = 3000
+            });
+            await toast.FireAsync(icon: SweetAlertIcon.Success, message: "Registro creado con éxito.");
+        }
+
+        private void Return()
+        {
+            stateForm!.FormPostedSuccessfully = true;
+            NavigationManager.NavigateTo($"/countries/details/{CountryId}");
+        }
+    }
+}
