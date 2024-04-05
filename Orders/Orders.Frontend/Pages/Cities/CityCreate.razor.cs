52f6e1b1c41b40babea10cc0b61b75f36dfec6ea
Juan Zuluaga <juan.zuluaga@cscgfm.com>
2024-04-04 19:13:44 -0500

    Create / Edit Cities

---
+++ Orders/Orders.Frontend/Pages/Cities/CityCreate.razor.cs
@@ -0,0 +1,46 @@
+﻿using CurrieTechnologies.Razor.SweetAlert2;
+using Microsoft.AspNetCore.Components;
+using Orders.Frontend.Repositories;
+using Orders.Frontend.Shared;
+using Orders.Shared.Entities;
+
+namespace Orders.Frontend.Pages.Cities
+{
+    public partial class CityCreate
+    {
+        private City city = new();
+        private FormWithName<City>? cityForm;
+
+        [Inject] private IRepository Repository { get; set; } = null!;
+        [Inject] private NavigationManager NavigationManager { get; set; } = null!;
+        [Inject] private SweetAlertService SweetAlertService { get; set; } = null!;
+        [Parameter] public int StateId { get; set; }
+
+        private async Task CreateAsync()
+        {
+            city.StateId = StateId;
+            var responseHttp = await Repository.PostAsync("/api/cities", city);
+            if (responseHttp.Error)
+            {
+                var message = await responseHttp.GetErrorMessageAsync();
+                await SweetAlertService.FireAsync("Error", message, SweetAlertIcon.Error);
+                return;
+            }
+            Return();
+            var toast = SweetAlertService.Mixin(new SweetAlertOptions
+            {
+                Toast = true,
+                Position = SweetAlertPosition.BottomEnd,
+                ShowConfirmButton = true,
+                Timer = 3000
+            });
+            await toast.FireAsync(icon: SweetAlertIcon.Success, message: "Registro creado con éxito.");
+        }
+
+        private void Return()
+        {
+            cityForm!.FormPostedSuccessfully = true;
+            NavigationManager.NavigateTo($"/states/details/{StateId}");
+        }
+    }
+}
---
+++ Orders/Orders.Frontend/Pages/Cities/CityEdit.razor.cs
@@ -0,0 +1,63 @@
+﻿using CurrieTechnologies.Razor.SweetAlert2;
+using Microsoft.AspNetCore.Components;
+using Orders.Frontend.Repositories;
+using Orders.Frontend.Shared;
+using Orders.Shared.Entities;
+using System.Net;
+
+namespace Orders.Frontend.Pages.Cities
+{
+    public partial class CityEdit
+    {
+        private City? city;
+        private FormWithName<City>? cityForm;
+
+        [Inject] private NavigationManager NavigationManager { get; set; } = null!;
+        [Inject] private IRepository Repository { get; set; } = null!;
+        [Inject] private SweetAlertService SweetAlertService { get; set; } = null!;
+
+        [Parameter] public int CityId { get; set; }
+
+        protected override async Task OnParametersSetAsync()
+        {
+            var responseHttp = await Repository.GetAsync<City>($"/api/cities/{CityId}");
+            if (responseHttp.Error)
+            {
+                if (responseHttp.HttpResponseMessage.StatusCode == HttpStatusCode.NotFound)
+                {
+                    Return();
+                }
+                var message = await responseHttp.GetErrorMessageAsync();
+                await SweetAlertService.FireAsync("Error", message, SweetAlertIcon.Error);
+                return;
+            }
+            city = responseHttp.Response;
+        }
+
+        private async Task SaveAsync()
+        {
+            var response = await Repository.PutAsync($"/api/cities", city);
+            if (response.Error)
+            {
+                var message = await response.GetErrorMessageAsync();
+                await SweetAlertService.FireAsync("Error", message, SweetAlertIcon.Error);
+                return;
+            }
+            Return();
+            var toast = SweetAlertService.Mixin(new SweetAlertOptions
+            {
+                Toast = true,
+                Position = SweetAlertPosition.BottomEnd,
+                ShowConfirmButton = true,
+                Timer = 3000
+            });
+            await toast.FireAsync(icon: SweetAlertIcon.Success, message: "Cambios guardados con éxito.");
+        }
+
+        private void Return()
+        {
+            cityForm!.FormPostedSuccessfully = true;
+            NavigationManager.NavigateTo($"/states/details/{city!.StateId}");
+        }
+    }
+}
