744bb4da28ebec3811428c7c6311c529bdf6b6d4
Juan Zuluaga <juan.zuluaga@cscgfm.com>
2024-04-11 18:59:06 -0500

    Add user entity

---
+++ Orders/Orders.Shared/Enums/UserType.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace Orders.Shared.Enums
+{
+    public enum UserType
+    {
+        [Description("Administrador")]
+        Admin,
+
+        [Description("Usuario")]
+        User
+    }
+}
