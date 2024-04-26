fde9fc0f38128f94a3708dc865102b3b15244b5b
Juan Zuluaga <juan.zuluaga@cscgfm.com>
2024-04-25 20:07:43 -0500

    Change user password

--- Orders/Orders.Backend/Controllers/AccountsController.cs
+++ Orders/Orders.Backend/Controllers/AccountsController.cs
@@ -96,6 +96,31 @@ namespace Orders.Backend.Controllers
             return BadRequest(result.Errors.FirstOrDefault());
         }
 
+        [HttpPost("changePassword")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        public async Task<IActionResult> ChangePasswordAsync(ChangePasswordDTO model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var user = await _usersUnitOfWork.GetUserAsync(User.Identity!.Name!);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var result = await _usersUnitOfWork.ChangePasswordAsync(user, model.CurrentPassword, model.NewPassword);
+            if (!result.Succeeded)
+            {
+                return BadRequest(result.Errors.FirstOrDefault()!.Description);
+            }
+
+            return NoContent();
+        }
+
+
         [HttpPost("Login")]
         public async Task<IActionResult> LoginAsync([FromBody] LoginDTO model)
         {
---
+++ Orders/Orders.Frontend/Pages/Auth/ChangePassword.razor.cs
@@ -0,0 +1,40 @@
+using CurrieTechnologies.Razor.SweetAlert2;
+using Microsoft.AspNetCore.Components;
+using Orders.Frontend.Repositories;
+using Orders.Shared.DTOs;
+
+namespace Orders.Frontend.Pages.Auth
+{
+    public partial class ChangePassword
+    {
+        private ChangePasswordDTO changePasswordDTO = new();
+        private bool loading;
+
+        [Inject] private NavigationManager NavigationManager { get; set; } = null!;
+        [Inject] private SweetAlertService SweetAlertService { get; set; } = null!;
+        [Inject] private IRepository Repository { get; set; } = null!;
+
+        private async Task ChangePasswordAsync()
+        {
+            loading = true;
+            var responseHttp = await Repository.PostAsync("/api/accounts/changePassword", changePasswordDTO);
+            loading = false;
+            if (responseHttp.Error)
+            {
+                var message = await responseHttp.GetErrorMessageAsync();
+                await SweetAlertService.FireAsync("Error", message, SweetAlertIcon.Error);
+                return;
+            }
+
+            NavigationManager.NavigateTo("/editUser");
+            var toast = SweetAlertService.Mixin(new SweetAlertOptions
+            {
+                Toast = true,
+                Position = SweetAlertPosition.BottomEnd,
+                ShowConfirmButton = true,
+                Timer = 3000
+            });
+            await toast.FireAsync(icon: SweetAlertIcon.Success, message: "Contraseña cambiada con éxito.");
+        }
+    }
+}
