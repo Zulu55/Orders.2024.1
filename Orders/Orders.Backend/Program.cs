d3b9884f0e7ddf9a5e3095cabe6672ccec781ff2
Juan Zuluaga <juan.zuluaga@cscgfm.com>
2024-04-30 19:04:48 -0500

    Products index

--- Orders/Orders.Backend/Program.cs
+++ Orders/Orders.Backend/Program.cs
@@ -65,11 +65,13 @@ builder.Services.AddScoped(typeof(IGenericUnitOfWork<>), typeof(GenericUnitOfWor
 builder.Services.AddScoped<ICategoriesRepository, CategoriesRepository>();
 builder.Services.AddScoped<ICitiesRepository, CitiesRepository>();
 builder.Services.AddScoped<ICountriesRepository, CountriesRepository>();
+builder.Services.AddScoped<IProductsRepository, ProductsRepository>();
 builder.Services.AddScoped<IStatesRepository, StatesRepository>();
 builder.Services.AddScoped<IUsersRepository, UsersRepository>();
 
 builder.Services.AddScoped<ICategoriesUnitOfWork, CategoriesUnitOfWork>();
 builder.Services.AddScoped<ICitiesUnitOfWork, CitiesUnitOfWork>();
+builder.Services.AddScoped<IProductsUnitOfWork, ProductsUnitOfWork>();
 builder.Services.AddScoped<ICountriesUnitOfWork, CountriesUnitOfWork>();
 builder.Services.AddScoped<IStatesUnitOfWork, StatesUnitOfWork>();
 builder.Services.AddScoped<IUsersUnitOfWork, UsersUnitOfWork>();
---
+++ Orders/Orders.Backend/UnitsOfWork/Interfaces/IProductsUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using Orders.Shared.DTOs;
+using Orders.Shared.Entities;
+using Orders.Shared.Responses;
+
+namespace Orders.Backend.UnitsOfWork.Interfaces
+{
+    public interface IProductsUnitOfWork
+    {
+        Task<ActionResponse<Product>> GetAsync(int id);
+
+        Task<ActionResponse<IEnumerable<Product>>> GetAsync(PaginationDTO pagination);
+
+        Task<ActionResponse<int>> GetTotalPagesAsync(PaginationDTO pagination);
+
+        Task<ActionResponse<Product>> AddFullAsync(ProductDTO productDTO);
+
+        Task<ActionResponse<Product>> UpdateFullAsync(ProductDTO productDTO);
+    }
+}
