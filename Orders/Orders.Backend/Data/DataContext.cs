511cb504148b5de0272f7890dcadb422fe314c67
Juan Zuluaga <juan.zuluaga@cscgfm.com>
2024-05-09 19:39:24 -0500

    Add products entities

--- Orders/Orders.Backend/Data/DataContext.cs
+++ Orders/Orders.Backend/Data/DataContext.cs
@@ -13,6 +13,9 @@ namespace Orders.Backend.Data
         public DbSet<City> Cities { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Country> Countries { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+        public DbSet<ProductImage> ProductImages { get; set; }
         public DbSet<State> States { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -21,6 +24,7 @@ namespace Orders.Backend.Data
             modelBuilder.Entity<Category>().HasIndex(x => x.Name).IsUnique();
             modelBuilder.Entity<Country>().HasIndex(x => x.Name).IsUnique();
             modelBuilder.Entity<City>().HasIndex(x => new { x.StateId, x.Name }).IsUnique();
+            modelBuilder.Entity<Product>().HasIndex(x => x.Name).IsUnique();
             modelBuilder.Entity<State>().HasIndex(x => new { x.CountryId, x.Name }).IsUnique();
             DisableCascadingDelete(modelBuilder);
         }
